cff643f6b2b4f40f7d7cd5311dc68e3a135e9ad3
Maria <mmorrison1978@gmail.com>
2016-03-19 17:39:20 -0600

    added decisions

--- app/src/main/java/com/example/aubreyford/androidappgroupproject_fe3/Decision.java
+++ app/src/main/java/com/example/aubreyford/androidappgroupproject_fe3/Decision.java
@@ -3,18 +3,28 @@ package com.example.aubreyford.androidappgroupproject_fe3;
 /**
  * Created by HomeMariaHome on 3/17/16.
  */
+
+/**
+ * Created by HomeMariaHome on 3/17/16.
+ */
 public class Decision {
     private int id;
     private int user_id;
     private String title;
     private String category;
+    private String picA;
+    private String picB;
 
-    public Decision(int id, int user_id, String title, String category) {
+
+    public Decision(int id, int user_id, String title, String category, String photoURLA, String photoURLB) {
         this.id = id;
         this.user_id = user_id;
         this.title = title;
         this.category = category;
+        this.picA = picA;
+        this.picB = picB;
     }
+
 
 //    private Photo photos[];
 
@@ -48,6 +58,22 @@ public class Decision {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getPicA() {
+        return picA;
+    }
+
+    public void setPicA(String picA) {
+        this.title = picA;
+    }
+
+    public String getPicB() {
+        return picB;
+    }
+
+    public void setPicB(String picB) {
+        this.category = picB;
     }
 
 }
